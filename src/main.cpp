c62d4e8c453916250f6e8cf3c5d8c2e9f2df38ea
Berkay Arslan <berkay.arslan@bordatech.com>
2022-04-07 14:57:50 +0300

    test timepoint conversion

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
-#include <thread>
+#include <cmath>
+#include <chrono>
+// #include "timer.h"
+
+using CLOCK = std::chrono::high_resolution_clock;
+using timepoint = CLOCK::time_point;
+using millisecs = std::chrono::milliseconds;
+
+int divider = pow(10, 6);
 
 int main()
 {
+    // determine time_epoch
+    auto tp = CLOCK::now();
+    auto time_epoch = tp.time_since_epoch().count();
+    // create timepoint variable
+    auto t1 = CLOCK::now() + std::chrono::seconds(2);
+    auto t1_ms = t1.time_since_epoch().count();
+    auto deadline_ms = (t1_ms - time_epoch) / divider;
 
+    std::cout << "time_epoch: " << time_epoch << " ms\n";
+    std::cout << "t1_ms: " << t1_ms << " ms\n";
+    std::cout << "deadline_ms: " << deadline_ms << " ms\n";
+    // subtract timepoint and time_epoch
+    return 0;
 }
