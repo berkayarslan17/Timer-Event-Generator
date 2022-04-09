8d6a3aabed0ada3504f61075d007f6c71c9d9f7e
Berkay Arslan <berkay.arslan@bordatech.com>
2022-04-10 00:55:17 +0300

    Remove unnecessary code blocks

--- src/main.cpp
+++ src/main.cpp
@@ -15,28 +15,13 @@ void log_callback(int id, const std::string &logstr)
 
 int main()
 {
-    // determine time_epoch
-    // auto tp = CLOCK::now();
-    // auto time_epoch = tp.time_since_epoch().count();
-    // // create timepoint variable
-    // auto t1 = CLOCK::now() + std::chrono::seconds(2);
-    // auto t1_ms = t1.time_since_epoch().count();
-    // auto deadline_ms = (t1_ms - time_epoch) / divider;
-
-    // std::cout << "time_epoch: " << time_epoch << " ms\n";
-    // std::cout << "t1_ms: " << t1_ms << " ms\n";
-    // std::cout << "deadline_ms: " << deadline_ms << " ms\n";
-    // subtract timepoint and time_epoch
-
     my_timer timer;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    // std::this_thread::sleep_for(std::chrono::seconds(1));
     T0 = CLOCK::now();
 
     log_callback(-1, "main starting");
-    auto t1 = CLOCK::now() + std::chrono::milliseconds(300);
+    auto t1 = CLOCK::now() + std::chrono::seconds(1);
     auto t2 = t1 + std::chrono::seconds(1);
-    auto t3 = t2 + std::chrono::seconds(3);
-    auto t4 = t2 + std::chrono::seconds(2);
 
     /* TIMER TYPE 1 */
 
@@ -44,28 +29,15 @@ int main()
                          { log_callback(1, "callback str"); });
     timer.register_timer(t1, [&]()
                          { log_callback(2, "callback str"); });
-    timer.register_timer(t3, [&]()
+    timer.register_timer(millisecs(700), [&]()
                          { log_callback(3, "callback str"); });
-    timer.register_timer(t4, [&]()
+    timer.register_timer(t1 + millisecs(300), millisecs(500), [&]()
                          { log_callback(4, "callback str"); });
-
-    /* TIMER TYPE 2 */
-
-    timer.register_timer(millisecs(1000), [&]()
-                         { log_callback(5, "callback str"); });
-
-    /* TIMER TYPE 3 */
-
-    timer.register_timer(t1, millisecs(1000), [&]()
-                         { log_callback(6, "callback str"); });
-
-    /* TIMER TYPE 4 */
-
     timer.register_timer([&]()
                          {static int count = 0; return ++count < 3; },
                          millisecs(500), [&]()
-                         { log_callback(7, "callback str"); });
+                         { log_callback(5, "callback str"); });
 
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    return 0;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    log_callback(-1, "main terminating");
 }
